eb61becc05dc530ccd97e4aeb833bd93916ce3d4
cristo mathew <cristomathew@cristos-MacBook-Air.local>
2019-07-22 12:32:53 +0530

    2nd commit

--- website/urls.py
+++ website/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('users/', include('users.urls')),
+    path('contact/', include('sendemail.urls')),
 ]
 
 if settings.DEBUG:
